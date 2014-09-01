d17a627163a0216703f4fe393969c868294803cb
ade <adouggy@gmail.com>
2014-09-01 20:52:39 +0800

    dododo……

---
+++ src/me/promenade/algorithm/Test_9_Is_Post_Iterator.java
@@ -0,0 +1,38 @@
+package me.promenade.algorithm;
+
+import java.util.Stack;
+
+import me.promenade.algorithm.common.AlgorithmUtil;
+import me.promenade.algorithm.common.BinaryTreeNode;
+
+public class Test_9_Is_Post_Iterator {
+
+	public static void postIterator(BinaryTreeNode<Integer> root) {
+		if (root == null)
+			return;
+
+		Stack<BinaryTreeNode<Integer>> s = new Stack<>();
+		BinaryTreeNode<Integer> current = root;
+		BinaryTreeNode<Integer> preVisited = null;
+		while (current != null || !s.isEmpty()) {
+			while( current != null ){
+				s.push(current);
+				current = current.getLeft();
+			}
+			current = s.peek();
+			if( current.getRight() == null || current.getRight() == preVisited ){
+				System.out.println( current.getData() );
+				preVisited = current;
+				s.pop();
+				current = null;
+			}else{
+				current = current.getRight();
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		BinaryTreeNode<Integer> node = AlgorithmUtil.createTree();
+		postIterator(node);
+	}
+}
--- src/me/promenade/algorithm/common/AlgorithmUtil.java
+++ src/me/promenade/algorithm/common/AlgorithmUtil.java
@@ -49,4 +49,15 @@ public class AlgorithmUtil {
 		}
 		System.out.println();
 	}
+	
+	public static BinaryTreeNode<Integer> createTree() {
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
+		root.setLeft(new BinaryTreeNode<Integer>(6));
+		root.setRight(new BinaryTreeNode<Integer>(14));
+		root.getLeft().setLeft(new BinaryTreeNode<Integer>(4));
+		root.getLeft().setRight(new BinaryTreeNode<Integer>(8));
+		root.getRight().setLeft(new BinaryTreeNode<Integer>(12));
+		root.getRight().setRight(new BinaryTreeNode<Integer>(16));
+		return root;
+	}
 }
